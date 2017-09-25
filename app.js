06294f523a50f254f0d255ea1a3bc4b0804992ee
edyluisrey <eaguirre378@gmail.com>
2017-09-25 01:08:21 -0500

    add jwt

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+var cors = require('cors');
 
 var MongoClient = require('mongodb').MongoClient;
 var dbUrl ="mongodb://mwa:mwa@ds147884.mlab.com:47884/mwa";
@@ -23,6 +26,41 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Enabling CORS
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+//JWT 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://edyluisrey.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'mwa',
+    issuer: "https://edyluisrey.auth0.com/",
+    algorithms: ['RS256']
+});
+app.use(jwtCheck);
 
 //midleware to connect database
 /*app.use((req, res, next)=>{
